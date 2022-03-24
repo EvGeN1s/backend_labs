084cf143f7782a2f94512efdf806de0c06d475d7
EvGeN1s <evgeniy.fedoseev.2002@gmail.com>
2022-03-24 17:26:11 +0300

    Add simple calculator

---
+++ lw1/Calculator/Calculator/Api/CalculatorService/CalculatorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.CalculatorApi
+{
+    class CalculatorApi
+    {
+        public static double Calculate(string command, double[] args)
+        {
+            switch (command)
+            {
+                case "+":
+                    return args[0] + args[1];
+                case "-":
+                    return args[0] - args[1];
+                case "*":
+                    return args[0] * args[1];
+                case "/":
+                    return args[0] / args[1];
+                default:
+                    throw new InvalidOperationException(command + " not found");
+                    
+            }               
+        }
+    }
+}
---
+++ lw1/Calculator/Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter command: ");
+            string command = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Enter args: ");
+            string calcualtedArgs = Console.ReadLine();
+
+            double[] argsArr = new double[2];
+            try
+            {
+                argsArr = parseArgs(calcualtedArgs);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid arguments");
+            }
+
+            try
+            {
+                double result = CalculatorApi.CalculatorApi.Calculate(command, argsArr);
+                Console.WriteLine("Your result: " + String.Format("{0:F2}", result));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Command");
+            }
+
+        }
+
+        private static double[] parseArgs(string args)
+        {
+            double[] result = new double[2];
+            int iterator = 0;
+
+            string argStr;
+            int start = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == ' ')
+                {
+                    argStr = args.Substring(start, i - start);
+                    start = i + 1;
+
+                    result[iterator] = Double.Parse(argStr);
+                    iterator++;
+                }
+
+                if (i + 1 == args.Length)
+                {
+                    argStr = args.Substring(start, i + 1 - start);
+
+                    result[iterator] = Double.Parse(argStr);
+                }
+            }
+
+            return result;
+        }
+    }
+}
